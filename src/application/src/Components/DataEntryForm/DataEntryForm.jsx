101b2410b12da27ecc8a76a70245e7f037125109
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-09-20 14:15:23 -0700

    Create initial data entry form for initial expense section

--- src/application/src/Components/DataEntryForm/DataEntryForm.jsx
+++ src/application/src/Components/DataEntryForm/DataEntryForm.jsx
@@ -1,18 +1,118 @@
-import React from "react";
-import "./DataEntryForm.css";
+import React, { useState } from 'react';
+import { NumericFormat } from 'react-number-format';
+import "./DataEntryForm.css"
 
-const DataEntryForm = () => {
+const ExpenseForm = () => {
+
+    const [salaries, setSalaries] = useState({
+        sales: '',
+        owners: '',
+        supervision_general: '',
+        supervision_parts: '',
+        supervision_office: '',
+        other_general: '',
+        other_office: '',
+    });
+
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Salaries:', salaries);
+    };
+
     return (
-        <div>
-            <div className="header">
-                <h1> Data Entry Form Draft 1 </h1>
-            </div>
+        <form onSubmit={handleSubmit}>
+            <h2>Data Entry - Expense Form</h2>
 
-            <div className="paragraph">
-                <p> This page will display the progress for the data entry input form for the financial data for the autobody shops.
-                </p>
-            </div>
-        </div>
+            {/* Salaries Section */}
+            <fieldset>
+                <legend>Salaries, Wages, Commissions</legend>
+                <div>
+                    <label>
+                        Compensation - Sales/Estimators
+                        <NumericFormat
+                            value={salaries.sales}
+                            thousandSeparator={true}
+                            prefix={'$'}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                setSalaries({...salaries, sales: floatValue});
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Compensation - Owners
+                        <NumericFormat
+                            value={salaries.owners}
+                            thousandSeparator={true}
+                            prefix={'$'}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                setSalaries({...salaries, owners: floatValue});
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Compensation - Supervision - General
+                        <NumericFormat
+                            value={salaries.supervision_general}
+                            thousandSeparator={true}
+                            prefix={'$'}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                setSalaries({...salaries, supervision_general: floatValue});
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Compensation - Supervision - Parts
+                        <NumericFormat
+                            value={salaries.supervision_parts}
+                            thousandSeparator={true}
+                            prefix={'$'}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                setSalaries({...salaries, supervision_parts: floatValue});
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Compensation - Supervision - Office
+                        <NumericFormat
+                            value={salaries.supervision_office}
+                            thousandSeparator={true}
+                            prefix={'$'}
+                            decimalScale={2}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                setSalaries({...salaries, supervision_office: floatValue});
+                            }}
+                        />
+                    </label>
+                </div>
+            </fieldset>
+
+            {/* Submit Button */}
+            <button type="submit">Submit</button>
+        </form>
     );
-}
-export default DataEntryForm;
+};
+
+export default ExpenseForm;
