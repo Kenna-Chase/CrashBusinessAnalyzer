032644b6379a436e4627fff7b7769b2758438d45
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-09-20 11:07:21 -0700

    Recover untracked js files

---
+++ src/application/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import LoginSignUp from "./Components/LoginSignUp/LoginSignUp";
+
+function App() {
+    return (
+        <div>
+            <LoginSignUp/>
+        </div>
+    );
+}
+
+export default App;
