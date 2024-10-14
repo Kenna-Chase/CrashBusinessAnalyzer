fe0b4b494952500c8de5fa178a249defe78fcb15
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-10-13 23:46:44 -0700

    Create expenses section of data entry form

---
+++ src/application/src/Components/MultiStepForm/FormSections/Expenses/OccupancyExpenses.jsx
@@ -0,0 +1,118 @@
+import {NumericFormat} from "react-number-format";
+import React, {useContext} from "react";
+import expensesData from "./ExpensesStruct";
+import {Button} from "antd";
+import ExpensesFormContext from "./ExpensesFormContext";
+
+const OtherOperatingExpenses = () => {
+    const {next, prev } = useContext(ExpensesFormContext);
+    return (
+        <div>
+            <fieldset>
+                <legend> Occupancy Expenses </legend>
+                <div>
+                    <label>
+                        Rent
+                        (ACCT#:{expensesData.occupancyExpenses.rent.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.rent.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.rent.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Amortization of Leaseholds
+                        (ACCT#:{expensesData.occupancyExpenses.amoritizationOfLeaseholds.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.amoritizationOfLeaseholds.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.amoritizationOfLeaseholds.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Repairs of Real Estate
+                        (ACCT#:{expensesData.occupancyExpenses.repairsOfRealEstate.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.repairsOfRealEstate.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.repairsOfRealEstate.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Depreciation - Buildings and Improvements
+                        (ACCT#:{expensesData.occupancyExpenses.depreciationBuilding.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.depreciationBuilding.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.depreciationBuilding.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Taxes - Real Estate
+                        (ACCT#:{expensesData.occupancyExpenses.realEstateTaxes.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.realEstateTaxes.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.realEstateTaxes.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Insurance - Buildings And Improvements
+                        (ACCT#:{expensesData.occupancyExpenses.buildingInsurance.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.buildingInsurance.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.buildingInsurance.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Utilities
+                        (ACCT#:{expensesData.occupancyExpenses.amoritizationOfLeaseholds.accountNumber})
+                        <NumericFormat
+                            value={expensesData.occupancyExpenses.amoritizationOfLeaseholds.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.occupancyExpenses.amoritizationOfLeaseholds.value = floatValue;
+                            }}
+                        />
+                    </label>
+                </div>
+
+                <Button type={"default"} onClick={prev}>
+                    Back
+                </Button>
+                <Button type={"primary"} onClick={next}>
+                    Next
+                </Button>
+            </fieldset>
+            <br/>
+        </div>
+    );
+};
+export default OtherOperatingExpenses;
---
+++ src/application/src/Components/MultiStepForm/FormSections/Expenses/SalariesWagesCommissions.jsx
@@ -0,0 +1,118 @@
+import {NumericFormat} from "react-number-format";
+import React, {useContext} from "react";
+import expensesData from "./ExpensesStruct";
+import {Button} from "antd";
+import ExpensesFormContext from "./ExpensesFormContext";
+
+const SalariesWagesCommissions = () => {
+    const {next, prev } = useContext(ExpensesFormContext);
+    return (
+        <div>
+            <fieldset>
+                <legend> Salaries, Wages, Commissions (S.G.A) </legend>
+                <div>
+                    <label>
+                        Compensation - Sales/Estimators
+                        (ACCT#:{expensesData.salaries.sales.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.sales.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.sales.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation - Owners
+                        (ACCT#:{expensesData.salaries.owners.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.owners.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.owners.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation - Supervision - General
+                        (ACCT#:{expensesData.salaries.supervision_general.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.supervision_general.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.supervision_general.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation - Supervision - Parts
+                        (ACCT#:{expensesData.salaries.supervision_parts.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.supervision_parts.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.supervision_parts.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation - Supervision - Office
+                        (ACCT#:{expensesData.salaries.supervision_office.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.supervision_office.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.supervision_office.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation -Other - General
+                        (ACCT#:{expensesData.salaries.other_general.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.other_general.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.other_general.value = floatValue;
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Compensation -Other - Office (Clerical)
+                        (ACCT#:{expensesData.salaries.other_office.accountNumber})
+                        <NumericFormat
+                            value={expensesData.salaries.other_office.value}
+                            decimalScale={0}
+                            fixedDecimalScale={true}
+                            onValueChange={(values) => {
+                                const {floatValue} = values;
+                                expensesData.salaries.other_office.value = floatValue;
+                            }}
+                        />
+                    </label>
+                </div>
+
+                <Button type={"default"} onClick={prev}>
+                    Back
+                </Button>
+                <Button type={"primary"} onClick={next}>
+                    Next
+                </Button>
+            </fieldset>
+            <br/>
+        </div>
+    );
+};
+export default SalariesWagesCommissions;
