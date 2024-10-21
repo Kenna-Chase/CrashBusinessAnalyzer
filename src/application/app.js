7d3d03e86d3985c76a397e07d5eee2eeb26fddac
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-10-21 15:11:45 -0700

    Update app.js for access to client files

--- src/application/app.js
+++ src/application/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
+var fs = require('fs');
 const port = 8080;
 const app = express();
 
-// Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // REST endpoint example
 app.get('/api/test', function (req, res) {
     res.json({ message: 'The REST endpoint test is running!' });
+});
+
+app.get('/', function (req, res) {
+   console.log('At Home Page');
 });
 
 // For any other route, send the React app's index.html
