e8e42ee4b5d35ce013c95d12e1834ed8cd26797e
Kenna Chase <31677932+Kenna-Chase@users.noreply.github.com>
2024-10-21 16:09:11 -0700

    Adjust app.js and package.json for deployment

--- src/application/app.js
+++ src/application/app.js
@@ -4,20 +4,16 @@ var fs = require('fs');
 const port = 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // REST endpoint example
 app.get('/api/test', function (req, res) {
     res.json({ message: 'The REST endpoint test is running!' });
 });
 
-app.get('/', function (req, res) {
-   console.log('At Home Page');
-});
-
 // For any other route, send the React app's index.html
 app.get('*', function (req, res) {
-    res.json({ message: 'This is not the page you are looking for!' });
+    res.sendFile(path.join(__dirname, 'public/index.js'));
 });
 
 // Start the server
